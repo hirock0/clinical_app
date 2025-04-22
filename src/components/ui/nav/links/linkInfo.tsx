e2ab015a582a7cab649518cbd82709bcde4a31ad
hirock0 <hirockdutta0@gmail.com>
2025-04-22 18:39:36 +0600

    navbar almost completd

--- src/components/ui/nav/links/linkInfo.tsx
+++ src/components/ui/nav/links/linkInfo.tsx
@@ -2,22 +2,46 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaCaretDown } from "react-icons/fa";
 
-const LinkInfo = (props: any) => {
+const LinkInfo = ({ item }: { item: any }) => {
+  console.log(item?.href)
   return (
     <div>
       <Link href={"/healthcare"}>
         <div className=" flex flex-col items-center">
           <h1 className=" overflow-hidden flex flex-col">
-            <span>{props?.title}</span>
-
+            <span>{item?.title}</span>
             <span className=" bg-cyan-300 w-full h-0.5 -translate-x-[110%] group-hover:translate-x-0 transition-all"></span>
           </h1>
           <FaCaretDown size={20} className=" text-cyan-200" />
         </div>
       </Link>
-      <div className=" hidden group-hover:block group-hover:absolute group-hover:left-0 group-hover:right-0  z-50  group-hover:bg-white group-hover:h-[300px] rounded-lg overflow-hidden transition-all text-black ">
+      <div className=" hidden group-hover:block group-hover:absolute group-hover:left-0 group-hover:right-0  z-50  group-hover:bg-white group-hover:h-[300px] rounded-lg overflow-hidden transition-all text-slate-600 ">
         <div className=" flex w-full h-full">
-          <div className=" w-3/4 p-5 bg-red-100">as</div>
+          <div className=" w-3/4 p-5 ">
+            <div className=" w-1/4">
+              <h1 className=" text-4xl font-bold text-blue-900 tracking-tight">{item?.title}</h1>
+              <p>{item?.info?.descriptions}</p>
+
+              <Link href={""}>
+                <button className=" bg-blue-900 text-white px-4  py-2 rounded-sm">
+                  Learn more
+                </button>
+              </Link>
+            </div>
+
+            <div className="">
+              <div className="">
+                {
+                  item?.info?.links?.map((item:any,index:any)=>(
+                    <div key={index} className="">
+                        {item?.href}
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            
+          </div>
           <div className=" w-1/4 p-5 bg-slate-200">df</div>
         </div>
       </div>
