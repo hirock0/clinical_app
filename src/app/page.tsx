c1a9277fd6f726379dff751628e6f6f82dd82779
hirock0 <hirockdutta0@gmail.com>
2025-04-22 23:48:25 +0600

    testimonial updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-
 import ContactUs from "@/components/contact-us/ContactUs";
 import ExperienceNumber from "@/components/experience/ExperienceNumber";
 import Banner from "@/components/home/banner/banner";
 import Opportunity from "@/components/opportunity/Opportunity";
 import Soluation from "@/components/soluation/Soluation";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 const Home = () => {
   return (
     <main>
-      <Banner/>
-      <ExperienceNumber/>
-      <Soluation/>
-      <Opportunity/>
-      <ContactUs/>
+      <Banner />
+      <ExperienceNumber />
+      <Soluation />
+      <Opportunity />
+      <Testimonials />
+      <ContactUs />
     </main>
   );
 };
